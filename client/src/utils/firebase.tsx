bf46cc8cf9aa692c987863d456af4b64fc57280b
BuildTools <brian.phartnettjr@gmail.com>
2023-10-29 01:19:10 -0400

    testing

--- client/src/utils/firebase.tsx
+++ client/src/utils/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
+  apiKey: "AIzaSyAkj9TMBVO06JO_KwG7k42n3sYeZeRR_DU",
   authDomain: "ecommerce-site-584f2.firebaseapp.com",
   projectId: "ecommerce-site-584f2",
   storageBucket: "ecommerce-site-584f2.appspot.com",
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID
+  messagingSenderId: "326175334264",
+  appId: "1:326175334264:web:0cde3f0e621c8a4b8b728d",
+  measurementId: "G-GSG7P8HWD7"
 };
 
 export const app = initializeApp(firebaseConfig);
